4cebdc5b0cde3e280a6fafa7f77015743cebabb6
duvallar <duvallar@rose-hulman.edu>
2023-01-19 23:05:53 -0500

    Test all tables

---
+++ SQLscripts/checkAllTables.sql
@@ -0,0 +1,13 @@
+use CTW_DB
+go
+select * from Drill
+select * from PlaysOn
+select * from Person
+select * from Coaches
+select * from team
+select * from Position
+select * from stat
+select * from targets
+select * from HasStat
+select * from player
+select * from Coach
