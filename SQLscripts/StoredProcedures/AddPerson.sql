b8ea85995ad82ddc80c03e365f320de4fd788d5c
rhit-laneks <laneks@rose-hulman.edu>
2023-01-25 16:49:45 -0500

    fix

--- SQLscripts/StoredProcedures/AddPerson.sql
+++ SQLscripts/StoredProcedures/AddPerson.sql
@@ -2,7 +2,7 @@ USE CTW_DB
 GO
 -- Kaylee
 CREATE PROCEDURE AddPerson
-	@DOB date = null,
+	@DOB date,
 	@FName varchar(100) ,
 	@LName varchar(100),
 	@Username varchar(50),
@@ -16,21 +16,33 @@ BEGIN
 		RAISERROR('First or last name cannot be null or empty.', 14, 1)
 		RETURN 1
 	END
+	IF (@DOB is null OR @DOB = '') BEGIN
+		RAISERROR('Date of Birth cannot be null or empty.', 14, 1)
+		RETURN 2
+	END
 	IF (@Username is null OR @Password is null OR @Username='' OR @Password='') BEGIN
 		RAISERROR('Username or Password cannot be null.', 14, 1)
-		RETURN 2
+		RETURN 3
 	END
 	IF (@Salt is null Or @Salt ='') BEGIN
 		RAISERROR('Salt cannot be null.', 14, 1)
-		RETURN 3
+		RETURN 4
 	END
 	IF(@Username IN (select Username From Person))BEGIN
 		RAISERROR('This Username is already taken.', 14, 1)
-		RETURN 4
+		RETURN 5
 	END
 
 
     INSERT INTO Person(DOB, FName, LName, Username, [Password], [Type], Salt )
     VALUES (@DOB, @FName, @LName, @Username, @Password, @Type, @Salt)
+
+	IF(NOT EXISTS(Select @Username From Person Where Username = @Username))
+	BEGIN
+		RAISERROR('Person add unsuccessful.', 14, 1)
+		RETURN 6
+	END
+
+
 	RETURN 0
 END
