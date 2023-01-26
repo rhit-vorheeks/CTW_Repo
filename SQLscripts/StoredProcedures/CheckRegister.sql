5d838b7a01962f3268bfaa7d8cc8e72954b7530e
rhit-laneks <laneks@rose-hulman.edu>
2023-01-26 17:44:04 -0500

    fix

--- SQLscripts/StoredProcedures/CheckRegister.sql
+++ SQLscripts/StoredProcedures/CheckRegister.sql
@@ -1,18 +1,18 @@
 USE CTW_DB
 GO
 -- Kaylee
-CREATE PROCEDURE CheckRegister
+ALTER PROCEDURE CheckRegister
 	@Username varchar(50)
 
 	AS 
 	BEGIN
 		IF(@Username is null or @Username = '') BEGIN
-			RAISERROR('Must provide a username', 14, 1)
+			PRINT 'Must provide a username'
 			RETURN 1
 		END
 
 		IF(EXISTS(Select Username From Person Where Username = @Username)) BEGIN
-			RAISERROR('This username is taken', 14, 1)
+			PRINT 'This username is taken'
 			RETURN 2
 		END
 
