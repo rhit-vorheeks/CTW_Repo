e13fa497715425c4d8e8609ae27850c8ffd0c6be
rhit-laneks <laneks@rose-hulman.edu>
2023-01-29 14:48:48 -0500

    fix

--- SQLscripts/StoredProcedures/GetRoster.sql
+++ SQLscripts/StoredProcedures/GetRoster.sql
@@ -1,21 +1,17 @@
 USE CTW_DB
 GO
 
-CREATE Proc GetRoster
+ALTER Proc GetRoster
 	@CoachUsername varchar(50)
 
 AS
 
 BEGIN
-Declare @CoachID int
-SELECT @CoachID = ID FROM Person WHERE Username = @CoachUsername
-
-
-SELECT T.Name AS 'Team Name', P.FName AS 'First Name', P.Lname AS 'Last Name', P.Username as 'Username', PO.PositionName as 'Position'
+SELECT T.[Name] AS 'Team Name', P.FName AS 'First Name', P.Lname AS 'Last Name', P.Username as 'Username', PO.PositionName as 'Position'
 FROM Team T Join PlaysOn PO on T.ID = PO.TeamID
 JOIN Person P on P.ID = PO.PlayerID
 JOIN Coaches C on C.TeamID = T.ID 
-WHERE C.CoachID = @CoachID
-GROUP BY T.Name, P.Lname, P.FName, P.Username, PO.PositionName
-
+JOIN Person P2 on P2.ID = C.CoachID
+WHERE P2.Username = @CoachUsername
+GROUP BY T.[Name], P.Lname, P.FName, P.Username, PO.PositionName
 END
