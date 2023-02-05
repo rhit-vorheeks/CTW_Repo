a93504d7ae3392572915c1f72687ed6eb5ddbfa0
rhit-vorheeks <vorheeks@rose-hulman.edu>
2023-02-05 11:48:29 -0500

    new sproc

---
+++ SQLscripts/StoredProcedures/UpdatePlayerInfo.sql
@@ -0,0 +1,45 @@
+
+CREATE PROC UpdatePlayerInfo
+    @Username varchar(50),
+    @Height int = null,
+	@Weight int = null
+
+AS
+BEGIN
+	IF (@Username is null) BEGIN
+		PRINT('Username cannot be null.')
+		RETURN 1
+	END
+
+	IF (@Weight IS NOT NULL) BEGIN
+		IF (@Weight < 0) BEGIN
+			PRINT 'Weight cannot be negative'
+			Return 2
+		END
+	END
+
+	IF (@Height IS NOT NULL) BEGIN
+		IF (@Height < 0) BEGIN
+			PRINT 'Height cannot be negative'
+			Return 3
+		END
+	END
+
+	IF (@Username NOT IN(select Username From Person))
+	BEGIN 
+		PRINT('Username is not a registered person. Register person first.')
+		RETURN 4
+	END 
+
+	DECLARE @ID int 
+	SELECT @ID = ID From Person Where Username = @Username
+
+	IF (@ID NOT IN(select ID From Player))
+	BEGIN 
+		PRINT('ID is not a registered player. Register as player first.')
+		RETURN 5
+	END 
+	
+	Update Player SET Height = @Height, [Weight] = @Weight WHERE [ID] = @ID
+	RETURN 0
+END
